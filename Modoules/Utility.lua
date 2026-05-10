0a8e81af69c68cd51d4daed0770893ed5ed8500b
neo <128645227+SzNeo8083@users.noreply.github.com>
2026-05-10 17:53:09 +0200

    Create Utility.lua

---
+++ Modoules/Utility.lua
@@ -0,0 +1,29 @@
+local Utility_object = {}
+Utility_object.__index = Utility_object
+
+function Utility_object:QuickInstance(Class, Data) 
+    local Object = Instance.new(Class);
+    for Property, Value in pairs(Data) do 
+        Object[Property] = Value;
+    end;
+    return Object
+end;
+
+function Utility_object:RgbToHex(color)
+    local r = math.round(color.R * 255)
+    local g = math.round(color.G * 255)
+    local b = math.round(color.B * 255)
+    return ('%02X%02X%02X'):format(r, g, b)
+end
+
+function Utility_object:lerpColor(value, colorA, colorB)
+    value = math.clamp(value,0,100)
+    local t = value / 100
+    local r = math.round(colorA.R * 255 * (1 - t) + colorB.R * 255 * t)
+    local g = math.round(colorA.G * 255 * (1 - t) + colorB.G * 255 * t)
+    local b = math.round(colorA.B * 255 * (1 - t) + colorB.B * 255 * t)
+    return ('%02X%02X%02X'):format(r, g, b)
+end
+
+getgenv().Modules.Utility = Utility_object
+return Utility_object
