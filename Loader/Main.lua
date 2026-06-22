3851a9b223fe696179b46134e235fa0d1e87e40a
neo <128645227+SzNeo8083@users.noreply.github.com>
2026-06-22 23:53:22 +0200

    Update Main.lua
    luarmor, not luaprot

--- Loader/Main.lua
+++ Loader/Main.lua
@@ -1,8 +1,44 @@
 --hello i am the loader
 if game.GameId == 113491250 then -- pf
    print("Found Game Phnatom Forces")
-	loadstring(game:HttpGet("https://luaprot.net/api/v2/loaders/get/36259776683223037581"))()
+	local source = [[
+		loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/fb376ba3f7b4fb7815128503025cb019.lua"))()
+	]]
+	
+	local executor = string.lower(identifyexecutor and identifyexecutor() or "")
+	local threadSource = [[
+	    for _, func in getgc(false) do
+	        if type(func) == "function" and islclosure(func) and debug.getinfo(func).name == "require" and string.find(debug.getinfo(func).source, "ClientLoader") then
+	            ]] .. source .. [[
+	            break
+	        end
+	    end
+	]]
+	
+	local function runSource(runner, getAll)
+	    for _, actor in getAll() do
+	        runner(actor, threadSource)
+	    end
+	end
+	
+	if (string.find(executor, "wave") or string.find(executor, "choco")) then
+	    runSource(run_on_actor, get_deleted_actors)
+	elseif string.find(executor, "volt") then
+	    runSource(run_on_actor, getactors)
+	elseif string.find(executor, "potassium") then
+	    runSource(run_on_thread, getactorthreads)
+	elseif getfflag and (string.lower(tostring(getfflag("DebugRunParallelLuaOnMainThread"))) == "true") then
+	    loadstring(source)()
+	elseif setfflag then
+	    setfflag("DebugRunParallelLuaOnMainThread", "True")
+	
+	    if queue_on_teleport then
+	        queue_on_teleport(source)
+	    end
+	    
+	    game:GetService("TeleportService"):Teleport(game.PlaceId)
+	end
 elseif game.GameId == 7633926880 then -- bs
     print("Found Game BloxStrike")
-loadstring(game:HttpGet("https://luaprot.net/api/v2/loaders/get/89628485128130002419"))()
+	loadstring(game:HttpGet("https://api.luarmor.net/files/v4/loaders/35e09e24fc039b0d90ddd4ca30b0ede0.lua"))()
 end
